e66d7ccb0f50dd6325841c04b6a0c1b67531255b
samragi99 <143216421+samragi99@users.noreply.github.com>
2023-08-31 20:57:43 -0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import "./app.css";
+
 function App() {
   return <div>My Country My pride</div>;
 }
-
+<div>
+  <header>
+    <h1>Header</h1>
+  </header>
+  <main>
+    <div class="center-box">
+      <div class="box"></div>
+    </div>
+  </main>
+  <footer>
+    <p>Footer</p>
+  </footer>
+</div>;
 export default App;
