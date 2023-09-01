4c3c0fa36b5a6f040c56300802ad3b6daea5b582
Santona Subedi <santonasubedi@gmail.com>
2023-08-31 20:51:37 -0500

    migrate html and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,20 @@
+import "./App.css";
 function App() {
-  return <div> Hello React</div>;
+  return (
+    <div>
+      {" "}
+      <header>
+        <h1>Header</h1>
+      </header>
+      <main>
+        <div className="center-box">
+          <div className="box"></div>
+        </div>
+      </main>
+      <footer>
+        <p>Footer</p>
+      </footer>
+    </div>
+  );
 }
 export default App;
