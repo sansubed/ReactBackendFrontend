ddec70250ba2441c37bee375c95d3c59fcf49438
Bikalp Timalsina <bikalp16@gmail.com>
2023-09-01 17:59:06 -0700

    added jsx and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
+import "./App.css";
 function App() {
   return (
-  <div>
-
-    <header>
-      <h1>Header</h1>
-    </header>
-    <main>
-      <div className="center-box">
-        <div className="box"></div>
-      </div>
-    </main>
-    <footer>
-      <p>Footer</p>
-    </footer>
+    <div>
+      <header>
+        <h1>Header</h1>
+      </header>
+      <main>
+        <div className="center-box">
+          <div className="box"></div>
+        </div>
+      </main>
+      <footer>
+        <p>Footer</p>
+      </footer>
     </div>
-    )
+  );
 }
 
-export default App
+export default App;
