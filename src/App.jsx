8dd4646af3c9710b2b28dc1fd5f8a3e1a386a9e1
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2023-09-02 08:26:05 +0545

    css box model

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import styles from './App.module.css'
 
 function App() {
+  // React Fragment: <></>
+
+  // TODO
+  // Box model for inline element
+  // id for css module
   return (
-    <div>
-      <header className={styles.header}>
-        <h1>Header</h1>
-      </header>
-      <main className={styles.main}>
-        <div className={styles.centerBox}>
-          <div className={styles.box}></div>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <p>Footer</p>
-      </footer>
-    </div>
+    <>
+      <div className={styles.container}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, sed repellat numquam molestias, voluptatibus at quo sequi aliquid, sit qui ullam quasi. Deserunt velit reiciendis similique
+        ipsam at natus magni dolore ex, molestias deleniti, officiis atque ipsa nobis reprehenderit libero! Nobis maiores quia beatae soluta nemo non ipsam saepe quo, nisi alias quisquam cumque.
+        Ratione quidem earum temporibus voluptatum animi unde, ex impedit autem porro facilis distinctio eius eveniet quisquam eum blanditiis tempora. Voluptate ipsam earum blanditiis impedit eligendi
+        quasi non nostrum et molestias tempore doloremque, quia nemo incidunt numquam, recusandae temporibus aut. Modi, voluptatem officiis. Quis provident et mollitia!
+      </div>
+    </>
   )
 }
 
