7c5d7b2c675e08759291266646b36975a4be6369
shanti basnet <shanti.basnet@mnsu.edu>
2023-09-05 18:35:00 -0700

    added header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
-import styles from "./App.module.css";
+//create app component
+
 function App() {
-  return (
-    <div>
-      <header className={styles.header}>
-        <h1>Header</h1>
-      </header>
-      <main className={styles.main}>
-        <div className={styles.centerBox}>
-          <div className={styles.box}></div>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <p>Footer</p>
-      </footer>
-    </div>
-  );
+  return <div>Hello App</div>;
 }
 
 export default App;
