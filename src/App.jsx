910a1aca3cf0cc388883ff2bf16c03ca29093fd3
aniltimalsina <timalsinaanil96@gmail.com>
2023-09-05 21:34:27 -0400

    add header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,6 @@
-import styles from "./App.module.css";
+// create App component
 function App() {
-  return (
-    <div>
-      <header className={styles.header}>
-        <h1>Header</h1>
-      </header>
-      <main className={styles.main}>
-        <div className={styles.centerBox}>
-          <div className={styles.box}></div>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <p>Footer</p>
-      </footer>
-    </div>
-  );
+  return <div>Hello App</div>;
 }
 
 export default App;
