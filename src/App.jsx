c92bbbb210360c42d2fcd6b83b32ded2ba1bf7d4
Santona Subedi <santonasubedi@gmail.com>
2023-09-05 20:34:21 -0500

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
-import styles from "./App.module.css"; //javascript
+//Create App Component
 
 function App() {
-  return (
-    <div>
-      {/* interpolistic: below is the jsx -> combination of java and html */}
-      <header className={styles.header}>
-        <h1>Header</h1>
-      </header>
-      <main className={styles.main}>
-        <div className={styles.centerBox}>
-          <div className={styles.box}></div>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <p>Footer</p>
-      </footer>
-    </div>
-  );
+  return <div>Hello App</div>;
 }
 export default App;
+
+// import styles from "./App.module.css"; //javascript
+
+// const myVariable = "Hello World";
+// function App() {
+//   //React Fragment is used to creare the empty tag, <></> if you do not want to create extra element
+//   return (
+//     <>
+//       <h1>{myVariable}</h1>
+//       <div className={styles.box}>
+//         Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, nulla
+//         nostrum. Rem eum praesentium eligendi labore ex, optio, quidem ea ipsa
+//         provident laborum harum dignissimos veritatis, necessitatibus unde
+//         ratione obcaecati!
+//       </div>
+//     </>
+//   );
+// }
+// export default App;
---
+++ src/header.jsx
@@ -0,0 +1,7 @@
+//create component
+
+function Header() {
+  return <div>I am the header.</div>;
+}
+
+export default Header;
