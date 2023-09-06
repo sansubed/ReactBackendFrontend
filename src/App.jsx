b8d45282f14c9c8e90260f8f621b98f096a5782f
Madhav Dhital <mdhital@madhavs-mbp.mynetworksettings.com>
2023-09-05 22:02:02 -0400

    added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 // Creat app component 
 
+import Header from "./header"
+import Footer from "./footer"
+
+
 function App() {
-  return <div>Hello App</div>
+  return(
+  <>
+  <Header />
+ <div>Hello App</div> 
+ <Header />
+ <Footer />
+ </>
+ 
+  )
 }
+
 export default App
+
 
