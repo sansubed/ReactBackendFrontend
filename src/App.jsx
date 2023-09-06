201cdca79a48fa34753798a8fb381230f5f7c2c7
Narayanadhikari9 <Narayanadhikari9@gmail.com>
2023-09-05 20:36:39 -0500

    add new file

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,6 @@
-import './App.css'
+//Create App Component
 
 function App() {
-  return (
-    <div>
-      <header>
-        <h1>Header</h1>
-      </header>
-      <main>
-        <div className="center-box">
-          <div className="box"></div>
-        </div>
-      </main>
-      <footer>
-        <p>Footer</p>
-      </footer>
-    </div>
-  )
+  return <div>Hello App</div>
 }
-
 export default App
