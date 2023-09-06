5a8026afa20b1548f43e8c042e807768b9410f90
Madhav Dhital <mdhital@madhavs-mbp.mynetworksettings.com>
2023-09-05 21:38:46 -0400

    header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
-import './App.css'
-function App() {
-  return <div>
-   <header>
-      <h1>Header</h1>
-    </header>
-    <main>
-      <div className="center-box">
-        <div className="box"></div>
-      </div>
-    </main>
-    <footer>
-      <p>Footer</p>
-    </footer>
-  </div>
-}
+// Creat app component 
 
+function App() {
+  return <div>Hello App</div>
+}
 export default App
 
