ddbd4411f6617b1d5c45cf22728777f1d044e1f1
Narayanadhikari9 <Narayanadhikari9@gmail.com>
2023-09-06 20:05:11 -0500

    add home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 //Create App Component
-import Header from './header'
-import Footer from './footer'
-function App() { 
-  return 
-    <>
-      <header />
-      <div>Hello App</div> 
-      <footer />
-    </>
+import Header from "./header";
+import Footer from "./footer";
+function App() {
+  return;
+  <>
+    <header />
+    <div></div>
+    <footer />
+  </>;
 }
-export default App
+export default App;
