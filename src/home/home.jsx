6f456f0047e2643abe4a1bb6b502e3dc3373bbe5
Madhav Dhital <mdhital@madhavs-mbp.mynetworksettings.com>
2023-09-06 21:30:57 -0400

    created home folder

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,7 +1,7 @@
 import "./home.css"
 
 function Home() {
-  return <div className="Home">I am home component</div>
+  return <div className="home">I am home component</div>
 }
 export default Home
 
