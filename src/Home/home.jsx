6acd287fcf9cefc22bc5fd6aebc9764b45e68f2e
Santona Subedi <santonasubedi@gmail.com>
2023-09-08 20:48:57 -0500

    test folder add

--- src/Home/home.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./home.module.css";
-function Home() {
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.home}>I am Home.</div>
-    </div>
-  );
-}
-export default Home;
--- src/home/home.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./home.module.css";
-
-function Home() {
-  return <div className={styles.home}> I am Home</div>;
-}
-
-export default Home;
