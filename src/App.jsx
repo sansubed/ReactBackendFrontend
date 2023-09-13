5a7378d82b97fe9c9eb301219257b61e5a5e9a95
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2023-09-13 07:50:20 +0545

    array and object

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,25 @@ import Header from './header/header'
 import Home from './home/home'
 
 function App() {
-  const country = 'Nepal'
+  // array in JS
+  const countryArr = ['USA', 'India', 'Nepal', 'Canada']
+
+  console.log(countryArr)
+
+  const countryObj = {
+    currentCountry: 'USA',
+    homeCountry: 'Nepal',
+    futureCountry: 'Canada',
+  }
+
+  console.log(countryObj)
   return (
     <div>
       <Header />
       <Home />
+      {countryArr[3]}
+      {countryArr.length}
+      {countryObj.homeCountry}
       <Footer />
     </div>
   )
