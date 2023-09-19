d36ab4b2ed407e964c6680d8562e4f4e2e773ce5
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2023-09-19 05:58:56 +0545

    useState hook

--- src/components/home-body/home-body.jsx
+++ src/components/home-body/home-body.jsx
@@ -2,36 +2,15 @@ import styles from './home-body.module.css'
 import { useState } from 'react'
 
 function HomeBody() {
-  const [state, setState] = useState([1990, 1995, 2000, 2004, 2007, 2007])
+  const [state, setState] = useState('Nepal')
 
-  // const state = [1990, 1995, 2000, 2004, 2007, 2007]
-
-  // console.log(setState())
-
-  // [1990, 1995, 2000, 2004, 2007, 2007]
-  // [2000]
-
-  console.log(state)
+  console.log('Rendering')
 
   return (
     <div className={styles.home}>
-      {/* Optional Chaining Operator */}
-
-      {state?.map((date, i) => (
-        // onClick: event
-        <div className={styles.item} key={i}>
-          {date}
-
-          <button
-            onClick={function () {
-              console.log('You clicked date:', date, i)
-              setState((prevState) => prevState.filter((clickedDate) => clickedDate !== date))
-            }}
-          >
-            X
-          </button>
-        </div>
-      ))}
+      <div onClick={() => setState('USA')} className={styles.item}>
+        {state}
+      </div>
     </div>
   )
 }
