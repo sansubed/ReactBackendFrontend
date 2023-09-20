f7661ba06b857a216e1c27a913aeeb058e2e4edd
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2023-09-20 07:56:46 +0545

    mode toggle app

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,13 +1,29 @@
-import Footer from '../../components/footer/footer'
-import Header from '../../components/header/header'
-import HomeBody from '../../components/home-body/home-body'
+import { useState } from 'react'
+import styles from './home.module.css'
+
+// 🌙
 
 function Home() {
+  const [mode, setMode] = useState(true)
+
+  // let mode = true
+
+  console.log('Rendering Home')
+
+  // falsy values
+  // undefined, null, NaN, '', 0, false
+
+  // ternary operator
+  // mode ? styles.light : styles.dark
+  // condition ? value_for_true_condition : value_for_false_condition
+
   return (
     <>
-      <Header />
-      <HomeBody />
-      <Footer />
+      <div className={mode ? styles.light : styles.dark}>
+        <button onClick={() => setMode(!mode)} className={styles.modeIcon}>
+          {mode ? '🌞' : '🌙'}
+        </button>
+      </div>
     </>
   )
 }
