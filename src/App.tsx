09cfa13dcd0ed26f7d6db7353af0122c153a87f7
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2023-09-26 07:48:45 +0545

    use js code to ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-function App() {
-  const country = 'Nepal'
+import { useState } from 'react'
+import Detail from './components/detail'
 
-  return <div>{country}</div>
+function App() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [country, setCountry] = useState('Nepal')
+  console.log('Rendering App component')
+  console.log('Value of isLoading', isLoading)
+
+  function updateState() {
+    console.log('Running')
+    setIsLoading(true)
+  }
+
+  function updateCountry(newCountry) {
+    setCountry(newCountry)
+  }
+
+  return (
+    <div>
+      {console.log('JSX of App component is Running')}
+      <Detail loading={isLoading} update={updateState} updateCountry={updateCountry} />
+      Value of country in App component: {country}
+    </div>
+  )
 }
 
 export default App
