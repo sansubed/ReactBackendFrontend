ba787fe477d2ad2f47282ec2c5ba6035b3ec165e
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2023-09-30 06:38:37 +0545

    async await

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,46 @@ const apiUrl = 'https://restcountries.com/v3.1/all'
 
 function App() {
   const [countries, setCountries] = useState([])
+  const [error, setError] = useState()
 
-  console.log(countries)
+  console.log(error)
 
   useEffect(() => {
-    fetch(apiUrl)
-      .then((res) => res.json())
-      .then((data) => setCountries(data))
-      .catch((error) => console.log('You got error!', error))
+    // fetch(apiUrl)
+    //   .then((res) => res.json())
+    //   .then((data) => setCountries(data))
+    //   .catch((error) => console.log('You got error!', error))
+    console.log('before function')
+    // Immediately Invoked Function Expression (IIFE)
+    async function getCountries() {
+      // 18 -> synchronous code
+      console.log('Running async function')
+      try {
+        const res = await fetch(apiUrl)
+        // 22 -> asynchronous code
+        console.log('Got json response!')
+        const data = await res.json()
+
+        setCountries(data)
+      } catch (e) {
+        setError(e)
+      }
+    }
+
+    getCountries()
+
+    console.log('after function')
   }, [])
 
-  return (
-    <div>
-      {countries.map((country, index) => (
-        <div key={index}>
-          <img src={country.flags.png} alt="" />
-          <div> {country.name.common}</div>
-        </div>
-      ))}
-    </div>
+  return error ? (
+    <div>{error.message}</div>
+  ) : (
+    countries.map((country, index) => (
+      <div key={index}>
+        <img src={country.flags.png} alt="" />
+        <div> {country.name.common}</div>
+      </div>
+    ))
   )
 }
 
